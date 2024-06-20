127f4559a940b133d3330ef74a3a54cb8b6a2d90
rohit-vue <rohit.excel2011@gmail.com>
2024-06-20 18:09:06 +0530

    Spacing and Minor Issue

--- Server/app/Http/Controllers/SettingsWorkflowController.php
+++ Server/app/Http/Controllers/SettingsWorkflowController.php
@@ -1,37 +1,42 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\SettingsWorkflowTasks;
 use App\Models\SettingsWorkflowQualityControl;
 use Illuminate\Http\Request;
 
 class SettingsWorkflowController extends Controller
 {
-    public function tasks(Request $request){
-         
+    public function tasks(Request $request)
+    {
+
         $request->validate([
-           'task_name' => 'required|string|max:255',
-           'department' => 'required|string|max:255',
-       ]);
+            'task_name' => 'required|string|max:255',
+            'department' => 'required|string|max:255',
+        ]);
 
-       $task = new SettingsWorkflowTasks();
-       $task->task_name = $request->task_name;
-       $task->department = $request->department;
-       $task->save();
+        $task = new SettingsWorkflowTasks();
+        $task->task_name = $request->task_name;
+        $task->department = $request->department;
+        $task->save();
 
-       return response()->json([
-        'task' => $task,
-        'message' => 'Task created successfully',
-         ], 201);
+        return response()->json([
+            'task' => $task,
+            'message' => 'Task created successfully',
+        ], 201);
     }
 
-    public function show_tasks(){
+    public function show_tasks()
+    {
         $data = SettingsWorkflowTasks::get();
         return response()->json([
-            'data' => $data]);
+            'data' => $data
+        ]);
     }
 
-    public function qualityControl(Request $request){
+    public function qualityControl(Request $request)
+    {
         $request->validate([
             'type' => 'required|string|max:255',
             'check' => 'required',
@@ -44,18 +49,18 @@ class SettingsWorkflowController extends Controller
         $task->description = $request->description;
         $task->status = $request->status;
         $task->save();
- 
-        return response()->json([
-         'task' => $task,
-         'message' => 'Success',
-          ], 201);
-        }
 
-        public function show_qualityControl(){
-            $data = SettingsWorkflowQualityControl::get();
-            return response()->json([
-                'data' => $data
-            ]);
-        }
+        return response()->json([
+            'task' => $task,
+            'message' => 'Success',
+        ], 201);
     }
 
+    public function show_qualityControl()
+    {
+        $data = SettingsWorkflowQualityControl::get();
+        return response()->json([
+            'data' => $data
+        ]);
+    }
+}
